b86f3d55895dc7f0b07ac6238a803a27136065ff
Dwijendra Parashar <dwijendraparashartech@gmail.com>
2020-12-18 13:50:03 +0530

    addcommentdeletepost

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -19,7 +19,7 @@ const Home = ()=>{
       fetch('/like',{
         method:"put",
         headers:{
-          "content-Type":"application/json",
+          "Content-Type":"application/json",
           "Authorization":"Bearer"+localStorage.getItem("jwt")
         },
         body:JSON.stringify({
@@ -34,7 +34,7 @@ const Home = ()=>{
       fetch('/unlike',{
         method:"put",
         headers:{
-          "content-Type":"application/json",
+          "Content-Type":"application/json",
           "Authorization":"Bearer"+localStorage.getItem("jwt")
         },
         body:JSON.stringify({
@@ -42,8 +42,55 @@ const Home = ()=>{
         })
       }).then(res=>res.json())
       .then(result=>{
+       const newData= data.map(item=>{
+         if(item._id==result._id){
+           return result
+         }else{
+           return item
+         }
+       })
+      setData(newData)
+      }).catch(err=>{
+        console.log(err)
+      })
+    }
+
+    const makeComment= (text,postId)=>{
+      fetch('/comment',{
+        method:"put",
+        headers:{
+          "Content-Type":"application/json",
+          "Authorization":"Bearer"+localStorage.getItem("jwt")
+        },
+        body:JSON.stringify({
+          postId,
+          text
+        })
+        }).then(result=>{
+          console.log(result)
+          const newData= data.map(item=>{
+            if(item._id==result._id){
+              return result
+            }else{
+              return item
+            }           
+          })
+          setData(newData)
+        }).catch(err=>{
+          console.log(err)
+        })
+    }
+
+
+    const deletePost = (postid)=>{
+      fetch('/deletepost/${postid}',{
+        method:"delete",
+        headers:{
+          Authorization:"Bearer".localStorage.getItem("jwt")
+        }
+      }).then(res=>res.json())
+      .then(result=>{
         console.log(result)
-        // setData(result.posts)
       })
     }
     return(
@@ -51,19 +98,42 @@ const Home = ()=>{
           {
             data.map(item=>{
               return(
-                <div className="card home-card">
-                  <h5>{item.postedBy.name}</h5>
+                <div className="card home-card" key={item._id}>
+                  <h5>{item.postedBy.name}<i className="material-icons" style={{
+                    float:"right"
+                  }}
+                  >delete</i></h5>
                   <div className="card-image">
-                      <img src={item.photo}></img>
+                      <img src={item.photo}/>
                   </div>
                   <div className="card-content">
                   <i className="material-icons" style={{color:"red"}}>favorite</i>
-                  <i class="material-icons">thumb_up</i>
-                  <i class="material-icons">thumb_down</i>
+                  {item.likes.includes(item._id)
+                  ?
+                  <i className="material-icons"
+                      onClick={()=>{unlikePost(item._id)}}
+                      >thumb_down</i>
+                  :
+                  <i className="material-icons" 
+                      onClick={()=>{likePost(item._id)}}
+                      >thumb_up</i>
+                  }
                     <h6>{item.likes.length}likes</h6>
                     <h6>{item.title}</h6>
                     <p>{item.body}</p>
+                    {
+                      item.comments.map(record=>{
+                          return(
+                          <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy}</span>{record.text}</h6>
+                          )
+                      })
+                    }
+                    <form onSubmit={(e)=>{
+                      e.preventDefault()
+                      makeComment(e.target[0].value,item._id)
+                    }}>   
                     <input type="text" placeholder="add a comment"/>
+                    </form>
                   </div>
                 </div>   
               )
